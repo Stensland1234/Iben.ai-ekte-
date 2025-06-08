4973f8335096464d00f425e29c18745866be65a0
Stensland1234 <espen.i.stensland@gmail.com>
2025-06-08 14:56:26 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Velkommen til Iben.ai</h1>
+      <p>Dette er første versjon – full kraft kommer snart.</p>
+    </div>
+  );
+}
